0ef15fa1b4d55d6fa1e69dea2206ebf77bb639ef
Xkckdndnd <duongthithuytrang2402@gmail.com>
2023-12-30 22:29:26 +0700

    gif

--- gif/js/myJs.js
+++ gif/js/myJs.js
@@ -118,7 +118,6 @@ $(document).ready(function () {
       background: '#fff url("img/iput-bg.jpg")',
       backdrop: `
                     rgba(0,0,123,0.4)
-                    url("img/giphy2.gif")
                     left top
                     no-repeat
                   `,
